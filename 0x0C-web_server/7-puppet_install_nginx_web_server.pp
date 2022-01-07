dcc5fc5d808551bb71f3dbcbfd84c00df0c56aae
kateincoding <kateincoding@gmail.com>
2022-01-06 23:24:49 -0500

    [improve syntax & add youtube url]

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,17 +5,17 @@ package { 'nginx':
 }
 
 file { '/var/www/html/index.html':
-  content => 'Hellow World'
+  content => 'Hellow World',
 }
 
 file_line { 'redirection-301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://example.com/ permanent;'
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
-  ensure  => 'running',
-  require => Package['nginx']
+  ensure  => running,
+  require => Package['nginx'],
 }
