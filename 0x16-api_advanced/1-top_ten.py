5ca848dc635ae60a80ff0f571605743c81b2112b
kate <kateincoding@gmail.com>
2022-02-15 22:24:02 -0500

    [list the 10 posts titles]

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Top ten"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        r = requests.get(url, headers=headers)
+        try:
+            data = r.json().get("data")
+            children = data.get("children")
+            for each in children[:10]:
+                print(each.get("data").get("title"))
+        except Exception as e:
+            print(None)
+    print(None)
+    return None
