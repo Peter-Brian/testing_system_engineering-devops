4a15fd255e73f2506d0403cdf740ec32f900a285
kate <kateincoding@gmail.com>
2022-02-15 23:58:36 -0500

    [fix]

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,41 +3,50 @@
 import requests
 
 
-def count_words(subreddit, word_list, after="", word_dict={}):
+def count_words(subreddit, word_list, after="", word_dic={}):
     """Return a list of titles of all hot articles"""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'user-agent': 'MyAPI/0.0.1'}
     params = {'after': after}
 
-    if not word_dict:
+    if not word_dic:
         for word in word_list:
-            word_dict[word] = 0
+            word_dic[word] = 0
 
     if after is None:
-        word_list = [[k, v] for k, v in word_dict.items()]
+        word_list = [[key, value] for key, value in word_dic.items()]
         word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
         for w in word_list:
             if w[1]:
                 print("{}: {}".format(w[0].lower(), w[1]))
         return None
 
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    r = requests.get(url, headers=headers, params=params,
+                     allow_redirects=False)
 
-    if response.status_code == 200:
-        try:
-            data = response.json().get("data")
-            after = data.get("after", None)
-            children = data.get("children", None)
-            for each in children:
-                title = each.get("data").get("title", None)
-                lower = [s.lower() for s in title.split(' ')]
-
-                for w in word_list:
-                    word_dict[w] += lower.count(w.lower())
-        except Exception as e:
-            return None
-    else:
+    if r.status_code != 200:
         return None
 
-    return recurse(subreddit, hot_list, after)
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except Exception as e:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
