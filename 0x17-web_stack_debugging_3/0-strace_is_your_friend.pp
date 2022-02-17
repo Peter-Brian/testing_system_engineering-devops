3a4f4e0b2d52ba1f710ddcaf64633e7348d7bfd4
kate <kateincoding@gmail.com>
2022-02-16 23:01:42 -0500

    [a+x]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,8 @@
 # fix config file
-
 exec { 'fix phpp':
 environment => ['DIR=/var/www/wp-settings.php',
                 'FIX=phpp',
                 'NEW=php'],
 command     => 'sed -i s/$FIX/$NEW/g $DIR',
-path        => ['/bin', '/usr/bin'],
+provider    => shell,
 }
