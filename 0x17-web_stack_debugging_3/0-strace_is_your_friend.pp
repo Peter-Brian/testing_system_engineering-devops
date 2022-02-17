873aec0372838fd6ee2859f17e4da1deba0ccfb3
kate <kateincoding@gmail.com>
2022-02-16 22:26:25 -0500

    [puppet]

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,9 @@
 #fix config file
+
 exec { 'fix phpp':
   environment => ['DIR=/var/www/wp-settings.php',
                   'FIX=phpp',
                   'NEW=php'],
-  command     => 'sudo sed -i "s/$FIX/$NEW/g" $DIR',
+  command     => 'sudo sed -i s/$FIX/$NEW/g $DIR',
   path        => ['/bin'],
-  returns     => [0, 1]
 }
